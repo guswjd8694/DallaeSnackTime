16bcabac294364e5c3909944c79068618e631b54
김현정 <ajdrns8694@gmail.com>
2025-04-05 21:21:36 +0900

    :sparkles: 간식 전체 뿌리기 기능 추가

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        SnackDrop.dropSnack();
+        SnackDropper.dropSnack();
         Dallae.moveDallae();
+        SnackDropper.dropAllSnack();
     }
 }
--- src/SnackDropper.java
+++ src/SnackDropper.java
@@ -1,13 +1,16 @@
-public class SnackDrop {
+public class SnackDropper {
     static String[] LocationArr = {"[침대]", "[베개 밑]", "[달래 계단]", "[커튼 밑]", "[달래 방석]", "[화장실 발매트]", "[옷방 러그]", "[2층 문 앞]"};
     static String[] droppedSnacks = new String[5];
     static int snackCount = 0;
+    static int dropAllCount = 0;
+
+    static SnackLimitChecker checker = new SnackLimitChecker();
 
 
     static void dropSnack() {
         String location;
 
-        if (snackCount >= 5){
+        if (!checker.canDrop(snackCount)){
             System.out.println("더 이상 간식을 뿌릴 수 없습니다! 🙅🏻‍♀️");
             return;
         }
@@ -22,6 +25,22 @@ public class SnackDrop {
         snackCount++;
     }
 
+    static void dropAllSnack(){
+        if(!checker.canDrop(dropAllCount)){
+            System.out.println("🐷 간식 파티는 오늘 여기까지! 꿀꿀 달래 멈춰! 🖐🏻  🐶💨");
+            return;
+        }
+
+        for (String s : LocationArr) {
+            snackCount++;
+        }
+
+        dropAllCount++;
+        System.out.println("🌈 간식 파티 오픈! 모든 장소에 간식이 뿌려졌어요!! 🎉🍖");
+    }
+
+
+
     static String getRandomLocation(){
         int randomIndex = (int)(Math.random() * LocationArr.length);
         return LocationArr[randomIndex];
@@ -35,6 +54,5 @@ public class SnackDrop {
         }
         return false;
     }
-
 
 }
